33a354c8a316bc3f76885ed974ce9f416fed38f2
Gitdiel <122370356+Gitdiel@users.noreply.github.com>
2024-03-07 17:30:48 -0500

    Update project.py

--- project.py
+++ project.py
@@ -28,7 +28,11 @@ class Project:
             start_date (str): The start date of the project.
             end_date (str): The end date of the project.
         """
-        pass
+        self.project_id = project_id
+        self.name = name
+        self.description = description
+        self.start_date = start_date
+        self.end_date = end_date
 
     def assign_employee(self, employee):
         """
@@ -37,4 +41,4 @@ class Project:
         Args:
             employee (Employee): The employee to be assigned to the project.
         """
-        pass
+        self.employee = employee
